c1b1e0be195c5fb832e3e0600fde91fe40046e0f
Ragd Alhejaily <ralhejaily@ut.edu.sa>
2024-07-28 17:59:29 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {LoginComponent} from "./auth/login/login.component";
+import {StuBodyComponent} from "./home/stu-body/stu-body.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    LoginComponent, StuBodyComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
